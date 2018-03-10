c60b99ab1faa35e12bc4cb781329e5fc9f624a54
Chad Sebesta <csebesta@users.noreply.github.com>
2018-03-10 12:58:48 -0700

    Updates to main script

--- kali-linux-xmonad.sh
+++ kali-linux-xmonad.sh
@@ -2,11 +2,9 @@
 # Kali Linux ISO recipe for minimal rescue image
 # Written by Chad Sebesta
 
-set -o errexit
-
 # Update and install dependencies
 apt-get update
-apt-get install git live-build cdebootstrap devscripts stow -y
+apt-get install git curl live-build cdebootstrap devscripts stow graphicsmagick -y
 git clone git://git.kali.org/live-build-config.git
 cd live-build-config
 
@@ -14,13 +12,14 @@ cd live-build-config
 cat > kali-config/variant-default/package-lists/kali.list.chroot << EOF
 # Defaults suggested by kali documentation
 alsa-tools
-debian-installer-launcher
+#debian-installer-launcher
 kali-archive-keyring
 kali-debtags
-kali-defaults
-kali-menu
+#kali-defaults
+#kali-menu
 kali-root-login
 locales-all
+pulseaudio
 xorg
 
 # Desktop environment (slide dependencies)
@@ -40,6 +39,11 @@ rxvt-unicode
 stow
 vim
 EOF
+
+# Modify splash screen
+gm convert \
+	-size 640x480 xc:#002b36 \
+	./kali-config/common/bootloaders/grub-pc/splash.png
 
 # Set up slide
 git clone https://github.com/csebesta/slide \
